4294587715c938a1ba1933d0998ffeeece8b528e
Caleb Collert <c-collert.1@onu.edu>
2024-04-17 19:14:48 -0400

    Lab Problem 10.3 finished

--- P10-3/P10-3.cpp
+++ P10-3/P10-3.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 
+int smallestValue(int array[], int size);
+int highestValue(int array[], int size);
+int average(int array[], int size);
 int main() {
 	int scores[10];
 	int students;
@@ -11,4 +14,36 @@ int main() {
 		cout << "Enter score #" << i + 1 << ":";
 		cin >> scores[i];
 	}
+
+	cout << "lowest: " << smallestValue(scores, students) << endl;
+	cout << "highest: " << highestValue(scores, students) << endl;
+	cout << "average: " << average(scores, students) << endl;
+}
+
+int smallestValue(int array[], int size) {
+	int smallest = INT_MAX;
+	for (int i = 0; i < size; i++) {
+		if (array[i] < smallest) {
+			smallest = array[i];
+		}
+	}
+	return smallest;
+}
+int highestValue(int array[], int size) {
+	int largest = INT_MIN;
+	for (int i = 0; i < size; i++) {
+		if (array[i] > largest) {
+			largest = array[i];
+		}
+	}
+	return largest;
+}
+
+int average(int array[], int size) {
+	int average = 0;
+	for (int i = 0; i < size; i++) {
+		average += array[i];
+	}
+	average /= size;
+	return average;
 }
